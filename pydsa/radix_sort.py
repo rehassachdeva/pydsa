ef49e4b3b35333e3d4534352c6dc74e88c469b80
Rehas Sachdeva <aquannie@gmail.com>
2016-04-07 14:39:44 +0530

    Fixed PEP8 code style errors

--- pydsa/radix_sort.py
+++ pydsa/radix_sort.py
@@ -6,6 +6,7 @@ system i.e. base is taken to be 10.
 
 BASE = 10
 
+
 def radix_sort(a):
     """
     Sorts the list 'a' using Radix Sort algorithm.
@@ -14,12 +15,12 @@ def radix_sort(a):
     >>> a = [708, 4567, 3, 45, 911, 123, 57, 37]
     >>> radix_sort(a)
     [3, 37, 45, 57, 123, 708, 911, 4567]
-    
+
     """
- 
+
     max_element = max(a)
     n = len(a)
- 
+
     # Do counting sort for every digit. Note that instead
     # of passing digit number, exp is passed. exp is BASE^i
     # where i is current digit number
@@ -27,33 +28,33 @@ def radix_sort(a):
     while max_element//exp > 0:
         _counting_sort(a, exp, n)
         exp *= BASE
-        
+
     return a
+
 
 def _counting_sort(a, exp, n):
     """Performs counting sort on list of length 'n' passed
     as 'a' according to the digit at the 'exp' place value.
     """
- 
+
     output = [0] * (n)
     count = [0] * (BASE)
- 
+
     # Store count of occurrences of digits in
     # 'exp' place value for each element of 'a'.
     for i in range(0, n):
         index = (a[i] // exp)
-        count[ (index) % BASE ] += 1
- 
+        count[(index) % BASE] += 1
+
     # Change count[i] so that count[i] now contains actual
     # position of this digit in output array.
     for i in range(1, BASE):
         count[i] += count[i-1]
-    
+
     for i in range(n-1, -1, -1):
         index = (a[i] // exp)
-        output[ count[ (index) % BASE ] - 1] = a[i]
-        count[ (index) % BASE ] -= 1
+        output[count[(index) % BASE] - 1] = a[i]
+        count[(index) % BASE] -= 1
 
     for i in range(0, n):
         a[i] = output[i]
-
